ac8ba7f173fd923a5ac3202745eb3332f1b81cfa
StevanCODE <pop.stevan@gmail.com>
2020-09-25 12:59:56 +0200

    Updates

---
+++ precache-manifest.cce5a5632d41f07c2a9bd131c4ed07d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2a2efee79defab2dcdbbe46fc66435",
+    "url": "/LoL-Projekat/index.html"
+  },
+  {
+    "revision": "8c5c5e6ea009a51bc0cc",
+    "url": "/LoL-Projekat/static/css/main.49b48f58.chunk.css"
+  },
+  {
+    "revision": "04057c6f6e75ef450eca",
+    "url": "/LoL-Projekat/static/js/2.f334a2ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/LoL-Projekat/static/js/2.f334a2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5c5e6ea009a51bc0cc",
+    "url": "/LoL-Projekat/static/js/main.c993c4da.chunk.js"
+  },
+  {
+    "revision": "f4668a852a1f2933254b",
+    "url": "/LoL-Projekat/static/js/runtime-main.0aea37e6.js"
+  }
+]);
